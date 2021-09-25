4330ec9ffc025e4312e0dfeda4342345111c650f
inbal <inbal12343@gmail.com>
2021-09-25 15:11:14 +0300

    Make 0-dimension-search work

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-# Goal: Make the first example work (Isosceles triangle)
 #TODO:
 # V use csv module
 # V parse files to create facts in memory and only then write to file
 # V Go back to triangle example and make it work
+# V Make 0-dimension-search work (emit new 'known' fact
 # - make square example work
-# - Go back to triangle example and make sure it work
-# Numeric search\ hill climbing
-# Take care of "error loading file" errors in souffle (for example create empty files)
-# Should I do something more? listen to recording\ go over my notes
-# Take care of input - from spec to dl file/ facts files
-# Handle exception in case souffle has error (catch it somehow and return niec output)
+# - Numeric search\ hill climbing
+# - Take care of "error loading file" errors in souffle (for example create empty files)
+# - listen to recording\ go over my notes
+# - Take care of input - from spec to dl file/ facts files
+# - Handle exception in case souffle has error (catch it somehow and return niec output)
 # Questions:
 # Can two relations share the same fact?
 
@@ -18,11 +17,13 @@ import shutil
 import re
 import csv
 
-exercise_name = "triangle"
+SAMLPES = {"triangle": ["Y"], "square": ["C", "D"], "test": ["W", "Y"]}
+
+EXERCISE_NAME = "test"
 souffle_main_dir = "souffleFiles"
-souffle_in_dir = os.path.join(souffle_main_dir, exercise_name)
-script = os.path.join("tmpInput", exercise_name + ".dl")
-souffle_out_dir = os.path.join(souffle_main_dir, exercise_name)
+souffle_in_dir = os.path.join(souffle_main_dir, EXERCISE_NAME)
+script = os.path.join("tmpInput", EXERCISE_NAME + ".dl")
+souffle_out_dir = os.path.join(souffle_main_dir, EXERCISE_NAME)
 
 
 class Relation:
@@ -52,7 +53,6 @@ def run_souffle():
     os.system("souffle -F {souffle_in_dir} {script} -D {souffle_out_dir} -I {include_dir}".format(souffle_in_dir=souffle_in_dir, script=script, souffle_out_dir=souffle_out_dir, include_dir="."))
 
 def add_facts_to_relation(rel):
-    # TODO: Seperate to two phases: first create facts, then write to files (according to relations)
     # The function read outpus of last souffle run, look for new facts from 'make' relations, and create appropriate facts. Return True if a new fact was added
     is_new_object = False
 
@@ -90,28 +90,11 @@ def add_facts_to_relation(rel):
         if fact.is_new:
             fact.is_new = False
             csv_writer.writerow(fact.params + [fact.id])
-    """
-    for i, line in enumerate(lines):
-        # Check if we have already seen this object
-        if line in rel.facts:
-            continue
-        if rel.should_delete_symmetry:
-            a, b = get_obj_from_line(line)
-            symmetric_line = "{b}\t{a}\n".format(b=b, a=a)
-            if symmetric_line in rel.facts:
-                continue
-        # Create new object in the facts file
-        rel.facts.append(line)
-        is_new_object = True
-        line_to_write = "{params}\t{name}{id}\n".format(params=line.strip("\n"), name=rel.name.lower(), id=i)
-        in_file.write(line_to_write)
-    """ 
     in_file.close()
     return is_new_object
 
 def find_all_locations(obj_id):
     # Get an object id, return list of locations it's in
-    #TODO: Use csv module  here as well
     loci = []
     f = open(os.path.join(souffle_out_dir, "In.csv"), "r")
     csv_reader = csv.reader(f, delimiter="\t")
@@ -139,14 +122,16 @@ def get_best_locus(loci):
         if locus in loci:
             if dim == 0:
                 locus_from_dim0 = locus
+                best_dim = 0
             if dim == 1:
                 locus_from_dim1 = locus
+                best_dim = 1
     f.close()
     
     if locus_from_dim0:
-        return locus_from_dim0
+        return locus_from_dim0, best_dim
     if locus_from_dim1:
-        return  locus_from_dim1
+        return  locus_from_dim1, best_dim
 
 
 def create_folder():
@@ -170,7 +155,7 @@ def deductive_synthesis():
 
 def parse_spec():
     # Open spec file and parse it. Return  output variable if exist
-    # TODO: Create appropriate facts file from the input
+    # TODO: Create appropriate facts file\ dl file from the input
     with open("example.spec", "r") as f:
         for line in f.readlines():
             line = line.strip("\n")
@@ -186,25 +171,75 @@ def parse_spec():
 def move_input_to_folder():
     # This function copies the files in the input dir, to the dir which souffle reads from
     # Later we will get the input from spec and won't need this
-    in_dir = os.path.join("tmpInput", exercise_name)
+    in_dir = os.path.join("tmpInput", EXERCISE_NAME)
     for file_name in os.listdir(in_dir):
         if file_name.endswith(".facts"):
             shutil.copyfile(os.path.join(in_dir, file_name), os.path.join(souffle_in_dir, file_name))
 
+def best_locus_for_each_var(output_vars):
+    # Get all output vars, find best locus for each one and return a dict that saves it
+    locus_per_var = {}
+    for var in output_vars:
+        loci = find_all_locations(var)
+        best_locus, best_dim = get_best_locus(loci)
+        if best_locus:
+            print(var + " is inside this locus: " + best_locus)
+            locus_per_var[var] = (best_locus, best_dim)
+        else:
+            print("Couldn't find a locus for " + var)
+    return locus_per_var
+
+def get_best_output_var(output_vars, locus_per_var):
+    # Get output vars and a dict with their best locations. return  the locus of the best one (lowest dimension locus)
+    overall_best_dim = None
+    overall_best_var = None
+    overall_best_locus = None
+    for var in locus_per_var.keys():
+        cur_locus, cur_dim = locus_per_var[var]
+        if not overall_best_dim or (cur_dim < overall_best_dim):
+            overall_best_dim = cur_dim
+            overall_best_var = var
+            overall_best_locus = cur_locus
+    return overall_best_var, overall_best_locus, overall_best_dim
+    
+def is_search_completed(output_vars, locus_per_var):
+    # Get output vars and a dict with their best locations.
+    # If all vars have locus from 0 dimension then return True
+    for var in output_vars:
+        if var not in locus_per_var:
+            return False
+        # This is the locus dimension
+        if locus_per_var[var][1] != 0:
+            return False
+    return True
+    
+def deductive_synthesis_and_numeric_search(output_vars):
+    while True:
+        deductive_synthesis()
+        locus_per_var = best_locus_for_each_var(output_vars)
+        if is_search_completed(output_vars, locus_per_var):
+            print("Search completed")
+            return
+        var, locus, dim = get_best_output_var(output_vars, locus_per_var)
+        if not var:
+            print("Not found output var with known location")
+            return
+        if dim == 0:
+            # Emit a known fact for this var, and run the deductive part again
+            f = open(os.path.join(souffle_in_dir, "Known.facts"), "a")
+            csv_writer = csv.writer(f, delimiter="\t")
+            csv_writer.writerow([var])
+            f.close()
+            output_vars.remove(var)
+        else:
+            # TODO: Add  hill-climbing algorithm here
+            return
+
 def main():
     create_folder()
     #output_vars = parse_spec() # Currentlhy parse spec only gives the output variables
-    output_vars = ['Y']
+    output_vars = SAMLPES[EXERCISE_NAME]
     #move_input_to_folder()
-    deductive_synthesis()
-    for var in output_vars:
-        loci = find_all_locations(var)
-        best_locus = get_best_locus(loci)
-        if best_locus:
-            print(var + " is inside this locus: " + best_locus)
-        else:
-            print("Couldn't find a locus for " + var)
-        
+    deductive_synthesis_and_numeric_search(output_vars)
         
 main()
-#add_facts_to_relation(make_relations[0])
