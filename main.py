c53c7b39d65d007ea70e729ab2d37bcf57f30c9a
inbal <inbal12343@gmail.com>
2021-11-29 17:57:23 +0200

    Get input in spec format

--- main.py
+++ main.py
@@ -5,27 +5,46 @@ import os
 from sympy import *
 import hillClimbing
 
-exercise_name = "triangle"
-script = os.path.join("tmpInput", exercise_name + ".dl")
+# name: [predicate_name, list_of_vars]
+SAMPLES = {"triangle": [
+            ["dist", ("X",  "Y", "d")],
+            ["dist", ("Z",  "Y", "d")],
+            ["known", ("X",  Point(0, 0))],
+            ["known", ("Z",  Point(1, 0))],
+            ["known", ("d",  1)],
+            ["output",  ("Y")]
+        ],
+"square2": [
+            ["makeLine", ("A", "B")],
+            ["makeLine", ("B", "C")],
+            ["dist", ("A", "B", "d")],
+            ["dist", ("B", "C", "d")],
+            ["dist", ("C", "D", "d")],
+            ["rightAngle", ("A", "B", "C")],
+            ["known", ("A", Point(0, 0))],
+            ["known",  ("B", Point(1,  0))],
+            ["known", ("d", 1)]
+        ]
+}
 
-# Create input statements from the front
-statements = front.parse_input(script)
-# TODO: Move SAMPLES here
-exercise = synthesis.SAMPLES[exercise_name]
 
-
-print("Partial program is: ")
-partial_prog = synthesis.main(exercise, exercise_name=exercise_name, statements=statements)
-print(partial_prog)
+def skip_front(exercise_name):
+    # Allows to uses input from sample and  skip the front
+    statements=front.build_statements(SAMPLES[exercise_name])
+    return statements
 
 if __name__ == '__main__':
 
-    known = {'X': Point(0, 0), 'Z': Point(1, 0), 'd': 1}
-    rules = [
-        [':in', 'Y', ['circle(X, d)', 'circle(Z, d)']],
-        ['assert', 'Y', 'abs(dist(X, Y) - d) + abs(dist(Z, Y) - d)']
-    ]
-    print(partial_prog.known)
-    print(partial_prog.rules)
+    # known = {'X': Point(0, 0), 'Z': Point(1, 0), 'd': 1}
+    # rules = [
+    #     [':in', 'Y', ['circle(X, d)', 'circle(Z, d)']],
+    #     ['assert', 'Y', 'abs(dist(X, Y) - d) + abs(dist(Z, Y) - d)']
+    # ]
+    print("In main")
+    exercise_name = "pentagon"
+    statements = front.main(exercise_name=exercise_name)
+    partial_prog = synthesis.main(exercise_name=exercise_name, statements=statements)
+    print("Partial program is: ")
+    print(partial_prog)
     print("Perform numeric search")
     hillClimbing.hillClimbing(partial_prog.known, partial_prog.rules)
