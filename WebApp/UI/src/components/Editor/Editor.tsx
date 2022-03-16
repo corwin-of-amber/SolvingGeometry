8703a03b772ecd10900aad488d1d86302e97b895
Shachar Itzhaky <corwin.amber@gmail.com>
2022-03-16 22:22:09 +0200

    [ui] Added a CodeMirror 6 editor.
    For user input.

---
+++ WebApp/UI/src/components/Editor/Editor.tsx
@@ -0,0 +1,32 @@
+import assert from 'assert';
+import React from 'react';
+import { CodeMirror } from '../../infra/cm6-easy';
+import './Editor.css';
+
+
+class Editor extends React.Component<EditorProps> {
+    cmContainer: React.RefObject<HTMLDivElement>
+    cm?: CodeMirror
+
+    constructor(props: EditorProps) {
+        super(props);
+        this.cmContainer = React.createRef();
+    }
+
+    render() {
+        return <div className="editor-container" ref={this.cmContainer}></div>;
+    }
+
+    componentDidMount(): void {
+        var div = this.cmContainer.current;
+        assert(div)
+        this.cm = new CodeMirror(div);
+        this.cm.on('change', () => Promise.resolve().then(() =>
+            console.log(this.cm!.getValue())));
+    }
+}
+
+type EditorProps = { onChange: (ev: {value: string}) => void };
+
+
+export { Editor }
--- WebApp/UI/src/components/SideBar/SideBar.tsx
+++ WebApp/UI/src/components/SideBar/SideBar.tsx
@@ -8,6 +8,7 @@ import ListItemText from '@mui/material/ListItemText';
 import { FixedSizeList, ListChildComponentProps } from "react-window";
 import AutoSizer from "react-virtualized-auto-sizer";
 import Tooltip from '@mui/material/Tooltip';
+import { Editor } from '../Editor/Editor'; 
 
 
 function renderRow(props: ListChildComponentProps) {
@@ -26,22 +27,28 @@ export const SideBar = () => {
 
     const [userRules, setUserRules] = useState<string>('');
 
+    /*
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault();
         setUserRules(e.target.value);
-    }
+    }*/
+    const handleChange = (e: {value: string}) => {
+        setUserRules(e.value);
+    };
 
     return (
         <div className='sidebar'>
             <div className="user-input">
-                <h3 className="user-input-title">Please insert rules:</h3>
+                <h3 className="user-input-title">Problem Constraints</h3>
+                <Editor onChange={handleChange}/>
+                {/*
                 <TextField
                     id="user-input-textfield"
                     label="Your Rules"
                     placeholder='Insert rules here'
                     multiline
                     rows={10}
-                    onChange={handleChange}/>
+                onChange={handleChange}/> */}
                 <div className='solve-buttons'>
                     <Tooltip title="Add points to model" arrow>
                         <Button onClick={() => {alert('clicked');}} style={{backgroundColor:"white"}} variant="outlined">Compile</Button>
@@ -54,7 +61,7 @@ export const SideBar = () => {
                 
             </div>
             <div className="partial-prog">
-                <h3 className="partial-prog-title">Program Steps:</h3>
+                <h3 className="partial-prog-title">Program Steps</h3>
                 <div className='partial-prog-output'>
                     <AutoSizer>
                         {({ height, width } : { height:any, width:any }) => (
